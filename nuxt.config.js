a43ad8fdd26fd9f7442c965e6181d1b521edf235
devJang <devjang2016@gmail.com>
2020-09-22 23:45:56 +0900

    feat: add styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,30 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-realworld',
+    title: 'Conduit',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+      },
+      {
+        type: 'text/css',
+        rel: 'stylesheet',
+        href:
+          '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
