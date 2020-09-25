eb74e4b0a6e4b37ac7639b5fee4f63f50de49217
devJang <devjang2016@gmail.com>
2020-09-25 23:48:56 +0900

    build: add axios-module

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: ['@nuxt/http'],
+  modules: ['@nuxtjs/axios', '@nuxt/http'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
