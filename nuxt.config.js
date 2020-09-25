3764154e5ef1b6cdbda8f129e9db8cc64882d7c1
devJang <devjang2016@gmail.com>
2020-09-25 23:46:52 +0900

    build: add @nuxt/http

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxt/http'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
