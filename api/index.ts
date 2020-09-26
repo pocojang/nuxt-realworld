71550908a0039414ba2aa7d13166bbacba8353b3
devJang <devjang2016@gmail.com>
2020-09-26 23:24:02 +0900

    feat: init repository design pattern

---
+++ api/index.ts
@@ -0,0 +1,5 @@
+import { NuxtAxiosInstance } from '@nuxtjs/axios'
+/**
+ * @see https://github.com/gothinkster/realworld/tree/3155494efe68432772157de38a90c49b3698897f/api
+ */
+export default ($axios: NuxtAxiosInstance) => ({})
--- nuxt.config.js
+++ nuxt.config.js
@@ -48,4 +48,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  axios: {
+    // Axios Configuration (https://github.com/nuxt-community/axios-module#options)
+    baseURL: 'https://conduit.productionready.io/api/',
+  },
 }
