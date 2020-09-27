5d0b5040163b2f1c81f0ff16f643b1468b721434
devJang <devjang2016@gmail.com>
2020-09-27 20:29:20 +0900

    build: install composition api

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   css: ['@/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/repository'],
+  plugins: ['@/plugins/repository', '@/plugins/composition-api'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
