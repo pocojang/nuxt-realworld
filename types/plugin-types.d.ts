5aa86e947c71d57384c3752a8acbb3cc100bbc6d
devJang <devjang2016@gmail.com>
2020-09-27 19:07:33 +0900

    feat: add typing plugin by repo to nuxt context

---
+++ types/plugin-types.d.ts
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { Repository } from '~/api'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $repository: Repository
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $repository: Repository
+  }
+  interface Context {
+    $repository: Repository
+  }
+}
