0e3ff691c088dc18a890645a3a76a7e95be363e1
devJang <devjang2016@gmail.com>
2020-09-29 23:40:50 +0900

    fix: typing

--- api/userRepository.ts
+++ api/userRepository.ts
@@ -2,20 +2,18 @@ import { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { User } from '~/types'
 
 type AuthLoginRequest = Pick<User, 'email'> & { password: string }
-type AuthRegisterRequest = Pick<User, 'username' | 'email'> & {
-  password: string
-}
+type AuthRegisterRequest = AuthLoginRequest & Pick<User, 'username'>
 type UpdateUserRequest = User & Pick<Partial<User>, 'email' | 'bio' | 'image'>
 
 // TODO: Response Model Typing
 export const userRepository = (axios: NuxtAxiosInstance) => ({
   authLogin(payload: AuthLoginRequest) {
     // No authentication required, returns a User
-    return axios.$post(`/users/login`, payload)
+    return axios.$post('/users/login', { user: payload })
   },
   authRegister(payload: AuthRegisterRequest) {
     // No authentication required, returns a User
-    return axios.$post(`/users`, payload)
+    return axios.$post(`/users`, { user: payload })
   },
   getCurrentUser() {
     // Authentication required, returns a User that's the current user
