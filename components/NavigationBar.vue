22629853f34ab7bb0644f290a33727bcb00c209c
devJang <devjang2016@gmail.com>
2020-10-04 23:33:49 +0900

    fix: link style of navigation bar

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,13 +1,21 @@
 <template>
   <ul class="nav navbar-nav pull-xs-right">
     <li class="nav-item">
-      <nuxt-link to="/" class="nav-link active"> Home </nuxt-link>
+      <nuxt-link to="/" class="nav-link"> Home </nuxt-link>
     </li>
     <li class="nav-item">
-      <nuxt-link to="/login" class="nav-link active"> Sign in </nuxt-link>
+      <nuxt-link to="/login" class="nav-link"> Sign in </nuxt-link>
     </li>
     <li class="nav-item">
-      <nuxt-link to="/register" class="nav-link active"> Sigin up </nuxt-link>
+      <nuxt-link to="/register" class="nav-link"> Sigin up </nuxt-link>
     </li>
   </ul>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NavigationBar',
+})
+</script>
