5f244bfdc4bf88f916fa4236ba1bc36d3a5b309e
devJang <devjang2016@gmail.com>
2020-10-08 23:28:58 +0900

    feat: add validation error component

---
+++ components/ErrorList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="error-messages">
+    <li v-if="(error, index) in errors" :key="index">
+      {{ error.key }} {{ error.value }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ErrorList',
+  props: {
+    errors: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
