1b7e044c2d1daddd6c4affca067abec57b7d3b2a
devJang <devjang2016@gmail.com>
2020-11-02 22:55:54 +0900

    fix: share state of use error

--- compositions/useError.ts
+++ compositions/useError.ts
@@ -1,20 +1,16 @@
 import { CustomErrors } from '@/types'
-import { onUnmounted, reactive } from '@nuxtjs/composition-api'
+import { reactive } from '@nuxtjs/composition-api'
 
 type State = Partial<CustomErrors>
 
-const state = reactive<State>({
-  errors: undefined,
-})
-
 export default function useError() {
+  const state = reactive<State>({
+    errors: undefined,
+  })
+
   const setError = (errors: State['errors']) => {
     state.errors = errors
   }
-
-  onUnmounted(() => {
-    setError(undefined)
-  })
 
   return {
     state,
